ff6a2e2a19202b927e71bb734add732c7f52a1ee
azmine-1 <sachchha222@gmail.com>
2025-06-15 23:50:04 -0400

    implementing string library for c

--- src/kernel/lib/string.c
+++ src/kernel/lib/string.c
@@ -6,5 +6,32 @@ size_t strlen(char* str){
         len++;
     }
     return len;
+}
 
+
+char* strcpy(char* dest, const char* src){
+    char* original_dest = dest;
+    while( *src != '\n'){
+        *dest = *src;
+        *dest++;
+        *src++;
+    }
+    *dest = '\0';
+    return original_dest;
+}
+
+char* strncpy(char* dest, const char* src, size_t n){
+    char* original_dest = dest;
+    while(n-- && (*dest++ = *src++));
+    while(n--)
+        *dest++ = '\0';
+    return original_dest;
+}
+
+int strcmp(char* str1, const char* str2){
+    while(*str1 &&(*str1 == *str2)){
+        *str1++;
+        *str2++;
+    }
+    return *(unsigned char*)str1 - *(unsigned char)str2;
 }
