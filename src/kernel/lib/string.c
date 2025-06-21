13d65f89911c203dc07e52bfd69f30017a09df13
azmine-1 <sachchha222@gmail.com>
2025-06-20 22:14:57 -0400

    memcmp implementation

--- src/kernel/lib/string.c
+++ src/kernel/lib/string.c
@@ -1,5 +1,5 @@
-#include "stdlib.h"
 #include "lib/string.h"
+
 size_t strlen(char* str){
     size_t len = 0;
     while(str[len]){
@@ -33,7 +33,7 @@ int strcmp(char* str1, const char* str2){
         *str1++;
         *str2++;
     }
-    return *(unsigned char*)str1 - *(unsigned char*)str2;
+    return *(unsigned char*)str1 - *(unsigned char)str2;
 }
 
 
@@ -53,52 +53,48 @@ char* strcat(char* dest, const char* src){
     while((*dest++ = *src++));
     return original_dest;
 }
-
-void* memset(void* ptr, int value, size_t num){
+void* memset(void* ptr, const void* src, size_t num){
+    
+}
+void* memcpy(void* ptr, const void* src, size_t num){
     unsigned char* p = (unsigned char*)ptr;
     while(num--){
-        *p++ = (unsigned char)value;
+        *p++ = (unsigned char*)
     }
     return ptr;
 }
 
-void* memcpy(void* dest, void* src, size_t num){
+void* memmove(void* dest, const void* src, size_t n){
     unsigned char* d = (unsigned char*)dest;
-    const unsigned char* s = (unsigned char*)src;
-    while(num--){
-        *d++ = *s++;
+    unsigned char* s = (unsigned char*)src;
+    
+    if(d==s || n==0){
+        return dest;
     }
-    return dest;
-}
-
-void* memmove(void* dest, void* src, size_t num){
-    unsigned char* d = (unsigned char*)dest;
-    const unsigned char* s =(unsigned char*)src;
 
     if(d < s){
-        while(num--){
-            *d++ = *s++;
+        for(size_t i =0, i<n; i++){
+            d[i] = s[i];
         }
-    } else if(d > s){
-        d += num - 1;
-        s += num -1; 
-        while(num--){
-            *d-- = *s--;
-        
+    }
+    if( d > s){
+        for( size_t i = n, i>0; i--){
+            d[i-1] = s[i-1];
         }
     }
     return dest;
-    
 }
 
+int memmcmp(void* ptr1, void* ptr2, int n){
+    unsigned char* p1 = ptr1;
+    unsigned char* p2 = ptr2; 
 
-int memcmp(const void* ptr1, const void* ptr2, size_t num){
-    const unsigned char* p1 = (const unsigned char*)ptr1;
-    const unsigned char* p2 = (const unsigned char*)ptr2;
-
-    while(num--){
-        if(*p1 != *p2){
-            return *p1 - *p2;
+    for(int i =0; i<n; i++){
+        if(*p1 > *p2){
+            return 1;
+        }
+        else if(*p1 < *p2){
+            return -1;
         }
         p1++;
         p2++;
